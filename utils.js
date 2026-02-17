842bc928521b5ad0f04c1c057d319973cdec246d
Ryan Pierce <ry@nspierce.com>
2026-02-17 09:19:09 -0700

    feat: enhance utils - better date format, debounce, improved slugify

--- utils.js
+++ utils.js
@@ -1,13 +1,31 @@
 function formatDate(date) {
-  return date.toISOString().split('T')[0];
+  return new Intl.DateTimeFormat('en-US', {
+    year: 'numeric',
+    month: 'long',
+    day: 'numeric',
+  }).format(date);
 }
 
-function generateId() {
-  return Math.random().toString(36).substring(2, 10);
+function generateId(prefix = '') {
+  const id = Math.random().toString(36).substring(2, 10);
+  return prefix ? `${prefix}_${id}` : id;
 }
 
 function slugify(text) {
-  return text.toLowerCase().replace(/\s+/g, '-').replace(/[^a-z0-9-]/g, '');
+  return text
+    .toLowerCase()
+    .trim()
+    .replace(/\s+/g, '-')
+    .replace(/[^a-z0-9-]/g, '')
+    .replace(/-+/g, '-');
 }
 
-module.exports = { formatDate, generateId, slugify };
+function debounce(fn, ms) {
+  let timer;
+  return (...args) => {
+    clearTimeout(timer);
+    timer = setTimeout(() => fn(...args), ms);
+  };
+}
+
+module.exports = { formatDate, generateId, slugify, debounce };
